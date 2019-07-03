e0200d99e6578a7446c2edac35d6b8a88b470baa
FeliOdras <ariane.feldmann@odras.de>
2019-07-03 13:32:32 +0200

    Added help text

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const fs = require("fs");
 
 const args = process.argv.slice(2);
+
 if (args.includes("ls") && args.includes("templates")) {
   const content = fs.readdirSync("./templates");
   console.log(content);
+  process.exit();
+}
+
+if (args.includes("--help")) {
+  console.log(`
+  Arguments:
+  --------------------------------------------
+  ls template => List all available templates
+  --------------------------------------------
+  --help => show help information
+  `);
+
+  process.exit();
 }
